b4b6a88639a6f870b518ab8180d476fdeb7b2b16
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-20 15:31:34 +0700

    4/7. Step 5 Look Back and Refactor.mp4

--- Problem_Solving/Add.js
+++ Problem_Solving/Add.js
@@ -1,21 +1,19 @@
 function charCount(str) {
     //make object to return at end
-    let result = {};
+    let obj = {};
 
     //loop over string, for each characters
-    for(let i = 0; i < str.length; i++) {
-        let char = str[i].toLowerCase();
-        //if the char is a key in object, add one to count
-        if(result[char] > 0) {
-            result[char]++;
-        } else {
-            //if the char is not in object, add it and set value to 1
-            result[char] = 1;
+    for(let char of str) {
+        char = char.toLowerCase();
+        //if alphabets/numbers
+        if(/[a-z0-9]/.test(char)){
+            //if the char is a key in object, add one to count
+            obj[char] = ++obj[char] || 1;//this is for obj[char] is truth it will do ++objchar and for falsy 1 
         }
     }
         //if char is something else (space, period, etc.) don't do anything 
     // return object at end
-    return result;
+    return obj;
 }
 
 console.log(charCount('hello'))//output {h:1, e:1, l:2, o:1}
