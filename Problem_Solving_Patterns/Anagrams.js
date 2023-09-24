5e3582a7d706307d4cb7d4ee28262cc8c8eccd1e
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-24 08:57:05 +0700

    5/6. Anagram Challenge Solution.mp4

---
+++ Problem_Solving_Patterns/Anagrams.js
@@ -0,0 +1,29 @@
+// ANagrams is given two strings, write a functionto determine if the second is an anagram of the first.
+function validAnargam(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    //if letter exists, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    //can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+console.log(validAnargam('anagram', 'nagaram'));//true
+console.log(validAnargam('cat', 'car'));//false
