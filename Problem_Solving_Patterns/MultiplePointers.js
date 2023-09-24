3ce7400c9682c09bc72a705ad3ba64e73472c1b5
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-24 11:17:59 +0700

    5/10. Count Unique Values Solution.mp4

--- Problem_Solving_Patterns/MultiplePointers.js
+++ Problem_Solving_Patterns/MultiplePointers.js
@@ -1,6 +1,7 @@
 //Multiple Pointers is creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
 
-function sumZero(arr) {//find sum zero in array
+function sumZero(arr) {
+  //find sum zero in array
   let left = 0;
   let right = arr.length - 1;
 
@@ -19,3 +20,24 @@ function sumZero(arr) {//find sum zero in array
 console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); //[-3, 3]
 console.log(sumZero([-5, -2, -1, 0, 1, 2, 4, 5])); //[-5, 5]
 console.log(sumZero([-3, -2, -1])); //undefined
+
+//unique values in array
+function countUniqueValues(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  var i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+
+  return i + 1;
+}
+
+console.log(countUniqueValues([1, 1, 2, 3, 3, 4, 5, 5, 6])); //6
+console.log(countUniqueValues([1, 1, 3])); //2
+console.log(countUniqueValues([])); //0
