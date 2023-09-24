2552f856f836c7c635d0fde7dca530afb4b3ac3d
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-24 12:51:14 +0700

    5/11. Sliding Window Pattern.mp4

---
+++ Problem_Solving_Patterns/SlidingWindows.js
@@ -0,0 +1,22 @@
+//Sliding Window is this pattern involves creating a window shich can either be an 
+
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+
+    for(let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+
+    tempSum = maxSum;
+
+    for(let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+} 
+
+console.log(maxSubarraySum([2, 6, 5, 3, 2, 4, 8, 9], 2))//17
+console.log(maxSubarraySum([2], 2))//null
