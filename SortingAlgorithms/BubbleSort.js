77f839fffea3f81199ac6eadb5f4a850a4937d2d
rasyid annas <rasshit.dsgn@gmail.com>
2023-10-10 19:45:30 +0700

    11/6. Bubble Sort Optimization.mp4

--- SortingAlgorithms/BubbleSort.js
+++ SortingAlgorithms/BubbleSort.js
@@ -1,17 +1,24 @@
 //Bubble Sort is a sorting algorithm where the largest values bubble up to the top!
-function bubbleSort(arr){
-    //this looping reverse because largest value done first and in last element
-    for(let i = arr.length; i > 0; i--) {
-        for(let j = 0; j < i - 1; j++) {
-            if (arr[j] > arr [j+1]) {
-                //SWAP!
-                let temp = arr[j];//this for save temporary
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
+function bubbleSort(arr) {
+  let noSwaps;//this set for SWAP
+
+  //this looping reverse because largest value done first and in last element
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        //SWAP!
+        let temp = arr[j]; //this for save temporary
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;//this will set noSwap to false if arr[j] > arr[j + 1]
+      }
     }
-    return arr;
+    if (noSwaps) break;
+  }
+  return arr;
 }
 
-console.log(bubbleSort([1, 17, 6, 40, 2]))
+console.log(bubbleSort([1, 17, 6, 40, 2]));
+console.log(bubbleSort([8, 3, 4, 5, 6]));
