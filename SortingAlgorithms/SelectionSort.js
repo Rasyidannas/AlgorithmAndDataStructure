e12fff39637072c62616f4a29ca721db78048eea
rasyid annas <rasshit.dsgn@gmail.com>
2023-10-19 09:03:44 +0700

    12/3. Selection sort implementation.mp4

---
+++ SortingAlgorithms/SelectionSort.js
@@ -0,0 +1,22 @@
+// Selection Sort is similar to bubble sort but instead of first placing large values into sorted position, it p;aces small values into sorted position.
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+        }
+
+        //changed place lowest to i (SWAPPING)
+        if(i !== lowest) {
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+
+    return arr;
+}
+
+console.log(selectionSort([23, 41, 2, 8, 10]));
