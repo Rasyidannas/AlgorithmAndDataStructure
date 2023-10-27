0f977e440d16f9b8021888b46d24140111fdcad6
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2023-10-27 01:41:00 +0000

    15/5. Merging arrays implementation.mp4

---
+++ Crazier_Sorting_Algorithms/MergeSort.js
@@ -0,0 +1,34 @@
+// Merge Sort is a combination of two things merging and sorting.
+
+//Example for merging arrys implementation
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  //   this is for rest element in arr1
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  //   this is for rest element in arr2
+  while (j < arr2.length) {
+    results.push(arr1[j]);
+    j++;
+  }
+
+  return results;
+}
+
+console.log(merge([4, 5, 9], [1, 3]));
