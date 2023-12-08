fefe630b772f19b5e647146b518093d41f58ab77
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2023-12-08 08:37:15 +0000

    18/107. Data Structures: Adding instance methods

--- Data_Structure/DataStructure&ClassIntro.js
+++ Data_Structure/DataStructure&ClassIntro.js
@@ -6,6 +6,30 @@ class Student {
     this.firstName = firstName; //this store as property (firstName, lastName, grade)
     this.lastName = lastName;
     this.grade = year;
+    this.tardies = 0;
+    this.scores = [];
+  }
+
+  //this are the methods
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate() {
+    this.tardies += 1;
+
+    if (this.tardies >= 3) {
+      return "YOU ARE EXPELLED!!!";
+    }
+
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies}`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAverage() {
+    const sum = this.scores.reduce((a, b) => a + b);
+    return sum / this.scores.length;
   }
 }
 
@@ -13,4 +37,9 @@ class Student {
 const student1 = new Student("Ciko", "Mentul", 4);
 
 console.log(student1);
-console.log(student1.firstName);
+console.log(student1.firstName); //this is for call property
+console.log(student1.fullName()); //this is for call methods
+console.log(student1.markLate()); //this is for call methods
+console.log(student1.addScore(12)); //this is for call methods
+console.log(student1.addScore(12)); //this is for call methods
+console.log(student1.calculateAverage()); //this is for call methods
