9342b978e8185b8582a25d1b69e3e2898767aea6
Rasyid <61853270+Rasyidannas@users.noreply.github.com>
2023-12-12 02:13:51 +0000

    19/111. Starter Code and Push Intro.mp4

---
+++ Data_Structure/SinglyLinkedLists.js
@@ -0,0 +1,33 @@
+//Singly Linked List a data structure that contains a head, tail and length property. Linked Lists consist of nodes, and each node has a value and a pointer to another node or null. A linked list is a collection of entities which are not stored in sequential order. Instead, each entity has a pointer to the next entity. Each entity, also referred to as a node, is composed of data and a reference (in other words, a link) to the next node in the sequence.
+
+//piece of data - val
+//reference to next node - next
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  //this is method for new node to the end of the Linked List
+  push() {}
+}
+
+//instantiate class and assign next node
+// var first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+
+var list = new SinglyLinkedList();
+list.push("Hello");
+
+console.log(first);
