392f291942741924aa27b9ef3c159e349a0d147b
Rasyid <61853270+Rasyidannas@users.noreply.github.com>
2024-01-01 06:01:12 +0000

    30/134. Push Solution.mp4

--- Data_Structure/DoubleLinkedLists.js
+++ Data_Structure/DoubleLinkedLists.js
@@ -13,4 +13,26 @@ class DoublyLinkdeList {
     this.tail = null;
     this.length = 0;
   }
+
+  //Adding a node to the end of the Doubly Linked List
+  push(val) {
+    var newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
+
+const list = new DoublyLinkdeList();
+list.push(99);
+list.push(100);
+console.log(list);
