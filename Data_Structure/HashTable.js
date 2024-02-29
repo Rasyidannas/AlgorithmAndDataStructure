5ed386940cefdcb16607c9d433e012bea25583b8
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2024-02-29 05:03:23 +0000

    25/198. Improving Our Hash Fuction.mp4

--- Data_Structure/HashTable.js
+++ Data_Structure/HashTable.js
@@ -3,11 +3,21 @@
 //hash function is convert keys into valid array indices
 function hash(key, arrayLen) {
   let total = 0;
-  for (let char of key) {
+  let WEIRD_PRIME = 31; //this is help full in spreading out key s more uniformly
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    //Math.min() will give limit if key.length more than 100
+    let char = key[i];
     //96 is from 'a' is 97 - 96 = 1, 'b' is 98 - 96 = 2, 'c' is 99 - 96 = 3
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
   }
 
   return total;
 }
+
+console.log(hash("hello", 13));
+console.log(hash("olleh", 13));
+console.log(hash("car", 13));
+console.log(hash("rac", 13));
+console.log(hash("cyan", 13));
+console.log(hash("pink", 13));
